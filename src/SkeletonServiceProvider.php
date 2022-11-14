1c1b8ac16e96dc8ac92ff8098d8aacb38ce8b7e5
liushoukun <liushoukun66@gmail.com>
2022-11-14 16:50:40 +0800

    项目修改

--- src/SkeletonServiceProvider.php
+++ src/SkeletonServiceProvider.php
@@ -2,24 +2,82 @@
 
 namespace VendorName\Skeleton;
 
-use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
-use VendorName\Skeleton\Commands\SkeletonCommand;
+use Illuminate\Support\ServiceProvider;
 
-class SkeletonServiceProvider extends PackageServiceProvider
+class SkeletonServiceProvider extends ServiceProvider
 {
-    public function configurePackage(Package $package): void
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot(): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
-        $package
-            ->name('skeleton')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_skeleton_table')
-            ->hasCommand(SkeletonCommand::class);
+        // $this->loadTranslationsFrom(__DIR__.'/../lang', ':vendor_slug.:package_slug');
+        // $this->loadViewsFrom(__DIR__.'/../resources/views', ':vendor_slug.:package_slug');
+        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        // $this->loadRoutesFrom(__DIR__.'/Routes/routes.php');
+
+        // Publishing is only necessary when using the CLI.
+        if ($this->app->runningInConsole()) {
+            $this->bootForConsole();
+        }
+    }
+
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/:package_slug.php', ':vendor_slug.:package_slug');
+
+        // Register the service the package provides.
+        $this->app->singleton(':vendor_slug.:package_slug', function ($app) {
+            return new Skeleton();
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [':vendor_slug.:package_slug'];
+    }
+
+    /**
+     * Console-specific booting.
+     *
+     * @return void
+     */
+    protected function bootForConsole(): void
+    {
+        // Publishing the configuration file.
+        $this->publishes([
+                             __DIR__.'/../config/skeleton.php' => config_path(':vendor_slug/:package_slug.php'),
+                         ], ':vendor_slug.:package_slug.config');
+
+        // Publishing the views.
+        /*$this->publishes([
+            __DIR__.'/../resources/views' => base_path('resources/views/vendor/:lc:vendor'),
+        ], ':vendor_slug.:package_slug.views');*/
+
+        // Publishing assets.
+        /*$this->publishes([
+            __DIR__.'/../resources/assets' => public_path('vendor/:lc:vendor'),
+        ], ':vendor_slug.:package_slug.views');*/
+
+        // Publishing the translation files.
+        /*$this->publishes([
+            __DIR__.'/../resources/lang' => resource_path('lang/vendor/:lc:vendor'),
+        ], ':vendor_slug.:package_slug.views');*/
+
+        // Registering package commands.
+        // $this->commands([]);
     }
 }
